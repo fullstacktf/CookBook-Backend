91b6f814474eece5718009c9e9ee938314262544
Carlos Arvelo <carvelog@gmail.com>
2019-12-04 10:59:19 +0000

    refactoring some files

--- app/users/controllers/user.controller.ts
+++ app/users/controllers/user.controller.ts
@@ -1,19 +1,19 @@
-import { User, IntUserModel } from '../models/users.model';
+import { User, UserModel } from '../models/users.model';
 
 
 export default class UserCRUD {
 
-    static async getAllUsers(): Promise<IntUserModel[]> {
+    static async getAllUsers(): Promise<UserModel[]> {
         const users = await User.find();
         return users;
     }
 
-    static async getUser(id: string): Promise<IntUserModel> {
+    static async getUser(id: string): Promise<UserModel> {
         const user = await User.findById(id);
         return user;
     }
-    
-    static async signUp(body): Promise<IntUserModel> {
+
+    static async signUp(body): Promise<UserModel> {
         const { userName, email, password } = body;
         const newUser = new User({
             userName,
@@ -23,8 +23,8 @@ export default class UserCRUD {
         //esto peta
         //newUser.password = await newUser.encryptPassword(password);
         await newUser.save();
-        return newUser;     
-    }     
+        return newUser;
+    }
 
     static async logIn(body): Promise<Boolean> {
         const existEmail = await User.findById(body.email);
@@ -38,12 +38,12 @@ export default class UserCRUD {
         }
     }
 
-    static async updateUser(id, body): Promise<IntUserModel> {
+    static async updateUser(id, body): Promise<UserModel> {
         const userUpdated = await User.findOneAndUpdate(id, body);
         return userUpdated;
     }
 
-    static async deleteUser(id: string, body): Promise<IntUserModel> {
+    static async deleteUser(id: string, body): Promise<UserModel> {
         const userDeleted = await User.findByIdAndDelete(id);
         return userDeleted;
     }
--- app/users/models/users.model.ts
+++ app/users/models/users.model.ts
@@ -1,65 +1,50 @@
 import { Document, Schema, Model, model } from 'mongoose';
+import { Hash } from 'crypto';
 //import { Hash } from 'crypto';
-//const { Schema } = mongoose;
-//const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
-export interface IntUserModel extends Document {
-    userName: string;
-    email: string;
-    password: string;
-    ingredientsPreferences: Array<string>;
-    follows: Array<string>;
-    ownRecipes: Array<string>;
-    otherRecipes: Array<string>;
-    rol: string
+export interface UserModel extends Document {
+  userName: string;
+  avatar: string
+  email: string;
+  password: string;
+  ingredientsPreferences: Array<string>;
+  follows: Array<string>;
+  ownRecipes: Array<string>;
+  otherRecipes: Array<string>;
+  rol: string
 }
 
 const UserSchema: Schema = new Schema({
-    userName: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    ingredientsPreferences: {
-        preferent: Array,
-        ignore: Array
-    },
-    follows: Array,
-    ownRecipes: Array,
-    otherRecipes: Array,
-    rol: String
+  userName: String,
+  avatar: String,
+  email: String,
+  password: String,
+  ingredientsPreferences: {
+    preferent: Array,
+    ignore: Array
+  },
+  follows: Array,
+  ownRecipes: Array,
+  otherRecipes: Array,
+  rol: String
 }, {
-    collection: 'UsersData'
+  collection: 'UsersData'
 });
 
+UserSchema.pre<UserModel>('save', function (next) {
+  if (!this.isModified('password')) return next()
 
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err)
 
+    bcrypt.hash(this.password, salt, null, (err, hash) => {
+      if (err) return next(err)
 
-// UserSchema.methods.encryptPassword = async (password) => {
-//     const salt = await bcrypt.genSalt(10);
-//     const hash = bcrypt.hash(password, salt);
-//     return hash;
-// };
+      this.password = hash
+      next()
+    })
+  })
+})
 
-// UserSchema.methods.matchPassword = async function (password) {
-//     return await bcrypt.compare(password, this.password);
-// };
-
-
-
-
-// UserSchema.pre('save', function (next) {
-//     const user = this;
-//     if (!user.isModified('password')) return next();
-
-//     bcrypt.genSalt(10, (err, salt) => {
-//         if (err) return next();
-
-//         bcrypt.hash(ser.password, salt, null, (err, hash) => {
-//             if (err) return next(err);
-
-//             user.password = hash;
-//             next();
-//         });
-//     });
-// });
-
-export const User: Model<IntUserModel> = model<IntUserModel>('users', UserSchema);
+export const User: Model<UserModel> = model<UserModel>('users', UserSchema);
