a49dcf513be50ba54c6276509b2c7ce1052b0ba6
Carlos Arvelo <carvelog@gmail.com>
2019-12-10 17:03:12 +0000

    modified .travis.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-COMMAND="cd ${REMOTE_PATH} && ls -la"
+scp -o StrictHostKeyChecking=no -i deploymentCookBook -v -r public ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
 
+#COMMAND="cd ${REMOTE_PATH} && ls -la"
 
-ssh -o StrictHostKeyChecking=no -i deploymentCookBook -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
+#ssh -o StrictHostKeyChecking=no -i deploymentCookBook -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
