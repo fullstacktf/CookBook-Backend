53a3fe0627b89866a8ee21e2c35405c5683fd875
Carlos Arvelo <carvelog@gmail.com>
2019-12-10 17:37:13 +0000

    modified .travis.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,6 @@
 
 scp -o StrictHostKeyChecking=no -i deploymentCookBook -v -r public ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
 
-#ssh -o StrictHostKeyChecking=no -i deploymentCookBook -v ${REMOTE_USER}@${REMOTE_HOST}
-
-#mv /project/public /var/www/chefs4.me/
 #COMMAND="cd ${REMOTE_PATH} && ls -la"
 
 #ssh -o StrictHostKeyChecking=no -i deploymentCookBook -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
