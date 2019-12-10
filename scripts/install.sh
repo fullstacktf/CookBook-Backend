e0dcb4d0c5fe7f7c8988ccb7019e1a968f0c009f
Carlos Arvelo <carvelog@gmail.com>
2019-12-10 01:32:08 +0000

    modified .travis.yml

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ echo "REMOTE_PORT = ${REMOTE_PORT}"
 echo "REMOTE_USER = ${REMOTE_USER}"
 echo "REMOTE_HOST = ${REMOTE_HOST}"
 
-openssl aes-256-cbc -K $encrypted_59fa64142762_key -iv $encrypted_59fa64142762_iv -in deploymentCookBook.enc -out deploymentCookBook -d
+openssl aes-256-cbc -K $encrypted_2ee26b6c1e49_key -iv $encrypted_2ee26b6c1e49_iv -in deploymentCookBook.enc -out deploymentCookBook -d
 
 eval "$(ssh-agent -s)"
 
